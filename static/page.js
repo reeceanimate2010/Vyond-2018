799786260cd0633e0419e47179a679209ffa2aa9
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-07-04 15:28:06 -0400

    Add files via upload

--- static/page.js
+++ static/page.js
@@ -179,82 +179,13 @@ module.exports = function (req, res, url) {
 			break;
 		}
 
-		case "/videos/": {
-			title = "Video Player - Vyond";
-			attrs = {
-				data: process.env.SWF_URL + "/player.swf",
-				type: "application/x-shockwave-flash",
-                                
-				id: "EmbedPlayer",
-                                width: "100%",
-                                height: "100%",
-
-                                align: "middle",
-                                allowScriptAccess: "always",
-                                allowFullScreen: "true",
-                                wmode: "transparent",
-
-                                hasVersion: "10.3",
-				quality: "medium"
-			};
-			params = {
-				flashvars: {
-					movieOwner: "African+Vulture%E2%84%A2",
-					movieOwnerId: "0uWOI2JiCdHU",
-					movieId: "",
-					movieLid: "0",
-					movieTitle: "Test",
-					movieDesc: "This+video+is+ment+for+testing+out+this+video+player.",
-					userId: "",
-					username:"", 
-					uemail: "",
-					ut: "-1",
-					numContact: "",
-					apiserver: "/",
-					duration: "92",
-					playcount: "0",
-					thumbnailURL: "https://s3.amazonaws.com/fs.goanimate.com/files/thumbnails/movie/1963/7876963/20609517L.jpg",
-					copyable: "0",
-					isPublished: "0",
-					ctc: "go",
-					tlang: "en_US",
-					is_private_shared: "1",
-					autostart: "0",
-					appCode: "go",
-					is_slideshow: "0",
-					originalId: "0zEt_fo4L-5k",
-					is_emessage: "0",
-					storePath: process.env.STORE_URL + "/<store>",
-					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-					animationPath: process.env.SWF_URL + "/",
-					isEmbed: "1",
-					refuser: null,
-					utm_source: null,
-					uid: null,
-					isTemplate: "0",
-					showButtons: "1",
-					chain_mids: "",
-					averageRating: 5,
-					ratingCount: "48",
-					fb_app_url: "/",
-					ad: 0,
-					endStyle: 0,
-					isWide: 0,
-					pwm: 1,
-					s3base: "https:\/\/s3.amazonaws.com\/fs.goanimate.com\/,https:\/\/assets.vyond.com\/"},
-				
-				movie: process.env.SWF_URL + "/player.swf", // 'http://localhost/player.swf'
-			};
-			break;
-		}
-
 		case "/go/savedMovie/": {
 			title = "View Or Record Your Movie - Vyond";
 			attrs = {
 				data: process.env.SWF_URL + "/player.swf",
 				type: "application/x-shockwave-flash",
                                 
-				id: "EmbedPlayer",
+				id: "Player",
                                 width: "100%",
                                 height: "100%",
 
@@ -296,7 +227,7 @@ module.exports = function (req, res, url) {
 					storePath: process.env.STORE_URL + "/<store>",
 					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
 					animationPath: process.env.SWF_URL + "/",
-					isEmbed: "1",
+					isEmbed: "0",
 					refuser: null,
 					utm_source: null,
 					uid: null,
@@ -311,39 +242,6 @@ module.exports = function (req, res, url) {
 					isWide: 0,
 					pwm: 1,
 					s3base: "https:\/\/s3.amazonaws.com\/fs.goanimate.com\/,https:\/\/assets.vyond.com\/"},
-				
-				movie: process.env.SWF_URL + "/player.swf", // 'http://localhost/player.swf'
-			};
-			break;
-		}
-		
-		case "/go/previewVideo/": {
-			title = "Preview Your Video - Vyond";
-			attrs = {
-				data: process.env.SWF_URL + "/player.swf",
-				type: "application/x-shockwave-flash",
-                                
-				id: "EmbedPlayer",
-                                width: "100%",
-                                height: "100%",
-
-                                align: "middle",
-                                allowScriptAccess: "always",
-                                allowFullScreen: "true",
-                                wmode: "transparent",
-
-                                hasVersion: "10.3",
-				quality: "medium"
-			};
-			params = {
-				flashvars: {
-					apiserver: "/",
-					storePath: process.env.STORE_URL + "/<store>",
-					ut: 60,
-					autostart: 1,
-					isWide: 1,
-					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
-				},
 				
 				movie: process.env.SWF_URL + "/player.swf", // 'http://localhost/player.swf'
 			};
