302d4bf003ae1c72b3fcdd5e66a076246aa20a1f
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-04 17:57:05 -0400

    Update pagelvmold.js

--- static/pagelvmold.js
+++ static/pagelvmold.js
@@ -125,7 +125,7 @@ module.exports = function (req, res, url) {
 <link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/common_combined.css.gz.css" rel="stylesheet" type="text/css">
 <link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/cc.css.gz.css" rel="stylesheet" type="text/css">
 <link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/video.css.gz.css" rel="stylesheet" type="text/css">
-<link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/studio.css.gz.css" rel="stylesheet" type="text/css">
+<link href="https://phpwrapper.herokuapp.com/pages/css/swf.css" rel="stylesheet" type="text/css">
 <link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/video_export.css.gz.css" rel="stylesheet" type="text/css">
 <link href="https://josephcrosmanplays532.github.io/static/55910a7cd204c37c/go/css/vm5_alpha/banner.css.gz.css" rel="stylesheet" type="text/css">
 <script>
