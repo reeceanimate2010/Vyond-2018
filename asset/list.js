d8fe404ae7ba155469964fb32072993b63e2e36e
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-14 12:06:52 -0400

    Update list.js

--- asset/list.js
+++ asset/list.js
@@ -14,7 +14,7 @@ async function listAssets(data, makeZip) {
 			xmlString = `${header}<ugc more="0">${chars
 				.map(
 					(v) =>
-						`<char id="${v.id}" name="${v.name}" cc_theme_id="${v.theme}" thumbnail_url="/char_thumbs/${v.id}.png" copyable="Y"><tags/></char>`
+						`<char id="${v.id}" name="Untitled" cc_theme_id="${v.theme}" thumbnail_url="http://localhost/char_thumbs/${v.id}.png" copyable="Y"><tags/></char>`
 				)
 				.join("")}</ugc>`;
 			break;
@@ -22,24 +22,26 @@ async function listAssets(data, makeZip) {
 		case "bg": {
 			var files = asset.list(data.movieId, "bg");
 			xmlString = `${header}<ugc more="0">${files
-				.map((v) => `<background subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y"/>`)
+				.map((v) => `<background subtype="0" id="${v.id}" name="${v.name}" enable="Y"/>`)
 				.join("")}</ugc>`;
 			break;
 		}
-		
 		case "sound": {
-				var files = asset.list(data.movieId, "sound");
-				xmlString = `${header}<ugc more="0">${files
-					.map((v) =>`<sound subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" duration="${v.duration}" downloadtype="progressive"/>`)
-					.join("")}</ugc>`;
-				break;
-		}
+			var files = asset.list(data.movieId, "voiceover");
+			xmlString = `${header}<ugc more="0">${files
+				.map(
+					(v) =>
+						`<sound subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" duration="${v.duration}" downloadtype="progressive"/>`
+				)
+				.join("")}</ugc>`;
+			break;
+		}	
 		case "movie": {
 			var files = asset.list(data.movieId, "starter");
 			xmlString = `${header}<ugc more="0">${files
 				.map(
 					(v) =>
-						`<movie id="${v.id}" path="/_SAVED/${v.id}" numScene="1" title="${v.name}" thumbnail_url="/movie_thumbs/${v.id}.png"><tags></tags></movie>`
+						`<movie id="${v.id}" enc_asset_id="${v.id}" path="/_SAVED/${v.id}" numScene="1" title="${v.name}" thumbnail_url="/movie_thumbs/${v.id}.png"><tags></tags></movie>`
 				)
 				.join("")}</ugc>`;
 			break;
@@ -48,7 +50,10 @@ async function listAssets(data, makeZip) {
 		default: {
 			var files = asset.list(data.movieId, "prop");
 			xmlString = `${header}<ugc more="0">${files
-				.map((v) =>`<prop subtype="${v.subtype}" id="${v.id}" name="${v.name}" enable="Y" holdable="${v.holdable}" headable="${v.headable}" placeable="1" facing="${v.facing}" width="${v.width}" height="${v.height}" duration="0"/>`)
+				.map(
+					(v) =>
+						`<prop subtype="0" id="${v.id}" name="${v.name}" enable="Y" holdable="0" headable="0" placeable="1" facing="left" width="0" height="0" duration="0"/>`
+				)
 				.join("")}</ugc>`;
 			break;
 		}
