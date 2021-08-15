148ac2c0b5ce889df0fb82461cbeb127ae8f0844
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-14 23:41:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const asu = require("./asset/upload");
 const stl = require("./static/load");
 const stp = require("./static/page");
 const scp = require("./static/pagelvmold");
+const srs = require("./static/pageretro");
+const srp = require("./static/pageretroplayer");
 const slp = require("./static/pagepreviewwindow");
 const stb = require("./static/pagelvp");
 const asl = require("./asset/load");
@@ -27,7 +29,7 @@ const tsv = require("./tts/voices");
 const tsl = require("./tts/load");
 const url = require("url");
 
-const functions = [mvL, pmc, asl, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, slp, scp, stb, stl,]; 
+const functions = [mvL, pmc, asl, chl, thl, thL, chs, cht, asL, tsl, chr, ast, mvm, mvl, mvs, mvt, tsv, asu, mvu, stp, slp, scp, srs, srp, stb, stl,]; 
 
 module.exports = http
 	.createServer((req, res) => {
