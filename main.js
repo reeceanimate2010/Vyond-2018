88afb7e96dbb1e7407a402d1da894ae42ee5e665
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-08-21 10:55:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+const RPC = require("discord-rpc");
 require("./server");
+
+const rpc = new RPC.Client({
+	transport: "ipc"
+});
+rpc.on("ready", () => {
+	rpc.setActivity({
+		state: "Using The Old Vyond Legacy Video Maker.",
+		startTimestamp: new Date(),
+		largeImageKey: "icon",
+		largeImageText: "Vyond 2018",
+		smallImageKey: "Vyond 2018",
+		smallImagetext: "Vyond 2018",
+	});
+	console.log("Rich presence is on!")
+});
+rpc.login({
+	clientId: "866340172874383370"
+}).catch(
+	console.log('RPC connection failed.')
+);
